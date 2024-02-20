dec07d474905bbd03637f2f9b3c122d3b3a6532e
VittIvanov <ivanov.vitaly.wdm@mail.ru>
2024-02-20 22:06:38 +0600

    i change header and burger-menu add to header(with css)

--- src/header/mainHeader.jsx
+++ src/header/mainHeader.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const MainHeader = () => {
   return (
     <React.Fragment>
-      <header className="App-header">
+      <header className="main-header">
         <div className="main-header--logo">
 
           <img className="header-logo__img" src={logo} alt="Описание вашего логотипа" />
@@ -13,10 +13,22 @@ const MainHeader = () => {
         </div>
 
 
-        <div>
-          <a href="*" className='visually-hidden'>
-            Contact Us
-          </a>
+        <div class="droplist">
+          <div class="droplist_burger-menu" id="burger-menu">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+          </div>
+          <ul class="droplist_menu active" id="menu">
+            <div class="droplist_in_burger-menu">
+              <div class="bar"></div>
+              <div class="bar"></div>
+              <div class="bar"></div>
+            </div>
+            <li><a href="#">Carrer</a></li>
+            <li><a href="#">Tender reports</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
         </div>
 
       </header >
