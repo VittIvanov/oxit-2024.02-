dfdd204f2764aa4983ce9eb91f1d818151f04baf
VittIvanov <ivanov.vitaly.wdm@mail.ru>
2024-02-21 19:48:02 +0600

    completed tayout of the burger-menu, added logic(mainMenu), wrot the layout(mainTitle), globaly added the Anton font

--- src/header/mainHeader.jsx
+++ src/header/mainHeader.jsx
@@ -1,8 +1,15 @@
 import logo from "../assets/home/headerLogo1.svg"
-import React from "react";
+import React, { useState } from "react";
 
 
 const MainHeader = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(prevState => !prevState);
+  }
+
   return (
     <React.Fragment>
       <header className="main-header">
@@ -14,21 +21,28 @@ const MainHeader = () => {
 
 
         <div class="droplist">
-          <div class="droplist_burger-menu" id="burger-menu">
+          <div class="droplist_burger-menu" id="burger-menu" onClick={toggleMenu}>
             <div class="bar"></div>
             <div class="bar"></div>
             <div class="bar"></div>
           </div>
-          <ul class="droplist_menu active" id="menu">
-            <div class="droplist_in_burger-menu">
+          <div class={
+            `droplist_menu ${menuOpen ? 'active' : ''
+            }`
+          } id="menu">
+            <div class="droplist_in_burger-menu" onClick={toggleMenu}>
               <div class="bar"></div>
               <div class="bar"></div>
               <div class="bar"></div>
             </div>
-            <li><a href="#">Carrer</a></li>
-            <li><a href="#">Tender reports</a></li>
-            <li><a href="#">Contact Us</a></li>
-          </ul>
+
+
+            <ul className="droplist_menu__list">
+              <li className="droplist_menu__item"><a href="#">Carrer</a></li>
+              <li className="droplist_menu__item"><a href="#">Tender reports</a></li>
+              <li className="droplist_menu__item"><a href="#">Contact Us</a></li>
+            </ul>
+          </div>
         </div>
 
       </header >
